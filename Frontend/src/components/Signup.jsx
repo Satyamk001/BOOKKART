78733861313fa5914a08be3f9dfc884b9fc88359
Satyam Kumar <103241507+Satyamk001@users.noreply.github.com>
2024-07-23 12:27:15 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,9 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      //https://ecomm-6r06.onrender.com
+      //http://localhost:4001
+      .post("https://ecomm-6r06.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
